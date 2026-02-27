0c9001a66168ed8f57dce0045f41007960a238cf
JesusRamon2192 <jesus.ramon2192@gmail.com>
2026-02-27 13:11:29 -0600

    allowHost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['rubik.jesusmalo.org'],
     proxy: {
       '/api': {
         target: 'http://backend:8080',
